f4aa4e2c3c4be67df27a8491a2c2f29e577cd2f9
Christopher Peters <righdforsa@gmail.com>
2019-11-25 22:51:44 +0000

    fixed after Bedrock changes

--- GameEngine.h
+++ GameEngine.h
@@ -7,6 +7,8 @@ static map<string, string> _gameState;
 class BedrockPlugin_GameEngine : public BedrockPlugin {
 
   public:
+    BedrockPlugin_GameEngine(BedrockServer& s);
+
     virtual string getName() { return "GameEngine"; }
     virtual bool peekCommand(SQLite& db, BedrockCommand& command);
     virtual void initialize(const SData& args, BedrockServer& server);
